fd2f4cebd8739abd48e7c252a0ac54fd84de7d24
DPlauder <docgit@gmail.com>
2024-02-01 12:42:49 +0100

    update vercel

--- components/NewViewPersonal.tsx
+++ components/NewViewPersonal.tsx
@@ -2,7 +2,7 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import React, { ChangeEvent } from "react";
+import { ChangeEvent } from "react";
 
 export interface Props {
   firstname: string;
